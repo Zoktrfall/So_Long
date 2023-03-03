77e254bcfb57bee81c5ff21bdf8d8ff620b7ca1c
Arman <armanmail@gmail.com>
2023-03-03 13:40:14 +0400

    error_empty

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include "so_long.h"
 
+void	error_empty(char **map)
+{
+	write(1, "Error\nYour File Is Empty\n", 25);
+	free_map(&map);
+	exit(0);
+}
+
 int	map_verification(char **map)
 {
 	if (rectangular_map(map))
@@ -14,6 +21,8 @@ int	args_verification(int argc, char **argv, t_mapdata *game)
 	if (correct_arguments(argc, argv))
 		return (1);
 	map = create_map(argv[1]);
+	if(map[0] == NULL)
+		error_empty(map);
 	map = optimization_map(map);
 	if (map_verification(map))
 	{
